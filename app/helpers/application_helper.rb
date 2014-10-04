dfdd65c180bc425a5f6453a8e23aa87a4a6d80e8
Jayzz55 <vagrant@rails-dev-box>
2014-10-04 15:00:37 +0000

    Install red carpet gem, and apply markdown to posts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,4 @@
- module ApplicationHelper
+module ApplicationHelper
    def form_group_tag(errors, &block)
      if errors.any?
        content_tag :div, capture(&block), class: 'form-group has-error'
@@ -6,4 +6,12 @@
        content_tag :div, capture(&block), class: 'form-group'
      end
    end
+
+  def markdown(text)
+    renderer = Redcarpet::Render::HTML.new
+    extensions = {fenced_code_blocks: true}
+    redcarpet = Redcarpet::Markdown.new(renderer, extensions)
+    (redcarpet.render text).html_safe
+  end
+
  end
