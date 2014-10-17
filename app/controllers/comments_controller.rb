a8b922b764f4e5a5ce9ad5b2b22db7168739e7c7
Jayzz55 <vagrant@rails-dev-box>
2014-10-17 01:44:48 +0000

    Apply shallow nesting on comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
   def create
-    @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:post_id])
+    @topic = @post.topic
     @comment = current_user.comments.build(comment_params)
     @comment.post = @post
     authorize @comment
@@ -17,18 +17,18 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-     @topic = Topic.find(params[:topic_id])
-     @post = @topic.posts.find(params[:post_id])
-     @comment = @post.comments.find(params[:id])
+    @post = Post.find(params[:post_id])
+    @topic = @post.topic
+    @comment = @post.comments.find(params[:id])
  
-     authorize @comment
-     if @comment.destroy
+    authorize @comment
+    if @comment.destroy
       flash[:notice] = "Comment was removed."
       redirect_to [@topic, @post]
-     else
+    else
       flash[:error] = "Comment couldn't be deleted. Try again."
       redirect_to [@topic, @post]
-     end
+    end
    end
 
   private
