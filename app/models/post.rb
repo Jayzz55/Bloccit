3298dc36a71cd18691e29a943904509299d79159
Jayzz55 <vagrant@rails-dev-box>
2014-10-18 03:55:21 +0000

    Apply Voting part 2

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@ class Post < ActiveRecord::Base
   belongs_to :user
   belongs_to :topic
 
-  default_scope { order('created_at DESC') }
+  default_scope { order('rank DESC') }
  
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
@@ -24,4 +24,18 @@ class Post < ActiveRecord::Base
      votes.sum(:value)
   end
 
+  def update_rank
+    age = (created_at - Time.new(1970,1,1)) / (60 * 60 * 24) # 1 day in seconds
+    new_rank = points + age
+ 
+    update_attribute(:rank, new_rank)
+  end
+
+  after_create :create_vote
+
+  private
+
+  def create_vote
+    user.votes.create(value: 1, post: self)
+  end
 end
