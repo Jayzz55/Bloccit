e47478e28d26be696ab2e3757138f53921a8a2e5
Jayzz55 <vagrant@rails-dev-box>
2014-10-27 13:59:11 +0000

    ajax flash implemented on AJAX

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,29 +9,30 @@ class ApplicationController < ActionController::Base
      redirect_to root_url, alert: exception.message
    end
 
+
   def after_sign_in_path_for(resource)
     topics_path
   end
+  
+def flash_headers
+  # This will discontinue execution if Rails detects that the request is not
+  # from an AJAX request, i.e. the header wont be added for normal requests
+  return unless request.xhr?
+ 
+  # Add the appropriate flash messages to the header, add or remove as
+  # needed, but I think you'll get the point
+  response.headers['x-flash'] = flash[:error]  unless flash[:error].blank?
+  response.headers['x-flash'] = flash[:notice]  unless flash[:notice].blank?
+  response.headers['x-flash'] = flash[:warning]  unless flash[:warning].blank?
+ 
+  # Stops the flash appearing when you next refresh the page
+  flash.discard
+end
   
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :name
-  end
-
-  def flash_headers
-    # This will discontinue execution if Rails detects that the request is not
-    # from an AJAX request, i.e. the header wont be added for normal requests
-    return unless request.xhr?
-   
-    # Add the appropriate flash messages to the header, add or remove as
-    # needed, but I think you'll get the point
-    response.headers['x-flash'] = flash[:error]  unless flash[:error].blank?
-    response.headers['x-flash'] = flash[:notice]  unless flash[:notice].blank?
-    response.headers['x-flash'] = flash[:warning]  unless flash[:warning].blank?
-   
-    # Stops the flash appearing when you next refresh the page
-    flash.discard
   end
   
 end
